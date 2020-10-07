4df74ed5e0dd8b396edbc29766be3e569e13d8e8
Macbook <alex@integra24.biz>
2020-10-07 20:36:33 +0700

    initial commit

---
+++ app/src/main/java/ru/int24/readdatamatrix/MainActivity.kt
@@ -0,0 +1,20 @@
+package ru.int24.readdatamatrix
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        id_scan.setOnClickListener{
+            val intent = Intent(this, ScanActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+}
